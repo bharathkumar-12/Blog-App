5c87fdaac46d3c36e075267b0243310d17fefa88
bharath kumar <bharathkumarakkiraju@gmail.com>
2020-10-23 10:01:24 +0530

    just extra css with bootstrap

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.views.generic import ListView, DetailView
+from django.views.generic.edit import CreateView
 from .models import Post
 
 class BlogListView(ListView):
@@ -12,3 +13,10 @@ class BlogDetailView(DetailView):
     template_name = "post_detail.html"
 
 # Create your views here.
+class BlogCreateView(CreateView):
+    model = Post
+    template_name = "post_new.html"
+    fields = ['title','author','body']
+
+    
+    
