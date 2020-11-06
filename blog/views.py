c251657c32bba2e81c86158be2761bc87d6ed9ff
bharath kumar <bharathkumarakkiraju@gmail.com>
2020-11-06 10:36:52 +0530

    improved creating and deleting posts

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
 from django.views.generic import ListView, DetailView
-from django.views.generic.edit import CreateView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from .models import Post
+from django.urls import reverse_lazy
 
 class BlogListView(ListView):
     model = Post
@@ -18,5 +19,14 @@ class BlogCreateView(CreateView):
     template_name = "post_new.html"
     fields = ['title','author','body']
 
-    
+class BlogUpdateView(UpdateView):
+    model = Post
+    template_name='post_edit.html'
+    fields=['title','body']
+
+class BlogDeleteView(DeleteView):
+    model = Post
+    template_name = 'post_delete.html'
+    fields = ['title']
+    success_url = reverse_lazy('home')
     
